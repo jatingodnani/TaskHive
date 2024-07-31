32b332a8bdad82f7344dc447de392a60a80ddb18
jatingodnani <godnanijatin@gmail.com>
2024-07-31 22:26:39 +0530

    Update CORS origin in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 dotenv.config();
 connectDB();
 app.use(cookieParser());
-app.use(cors({ origin: "https://task-hive-rfg2.vercel.app/", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/auth', router);
